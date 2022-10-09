4917f84c789313db5286e5882584d6dc83174bb2
Dmitriy Born <1111born1111@gmail.com>
2022-10-09 19:17:42 +0300

    Ex_1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("---------------Задача №1---------------");
+//Палиндром
+Console.WriteLine("Введите 5-значное число:");
+string N = Console.ReadLine()!;
+
+if (N.Length == 5){
+    if (N[0]==N[4] && N[1]==N[3]){
+        Console.WriteLine($"Число {N} является палиндромом");
+    }
+    else{
+        Console.WriteLine($"Число {N} не является палиндромом");
+    }
+}
+else{
+    Console.WriteLine($"Число {N} не является пятизначным");
+}
