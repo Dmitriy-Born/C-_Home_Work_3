00690c11e08aab74427d42fac449c332301b8583
Dmitriy Born <1111born1111@gmail.com>
2022-10-09 19:47:11 +0300

    коссентарии

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
-﻿// Console.WriteLine("---------------Задача №1---------------");
-// //Палиндром
-// Console.WriteLine("Введите 5-значное число:");
-// string N = Console.ReadLine()!;
+﻿Console.WriteLine("---------------Задача №1---------------");
+//Палиндром
+Console.WriteLine("Введите 5-значное число:");
+string N = Console.ReadLine()!;
 
-// if (N.Length == 5){
-//     if (N[0]==N[4] && N[1]==N[3]){
-//         Console.WriteLine($"Число {N} является палиндромом");
-//     }
-//     else{
-//         Console.WriteLine($"Число {N} не является палиндромом");
-//     }
-// }
-// else{
-//     Console.WriteLine($"Число {N} не является пятизначным");
-// }
+if (N.Length == 5){
+    if (N[0]==N[4] && N[1]==N[3]){
+        Console.WriteLine($"Число {N} является палиндромом");
+    }
+    else{
+        Console.WriteLine($"Число {N} не является палиндромом");
+    }
+}
+else{
+    Console.WriteLine($"Число {N} не является пятизначным");
+}
 
-// Console.WriteLine("---------------Задача №2---------------");
-// //Расстояние между точками
-// Console.WriteLine("Введите значение координаты X для точки А: ");
-// int xa = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите знакчение координаты Y для точки А: ");
-// int ya = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите значение координаты Z для точки А: ");
-// int za = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите значение координаты X для точки B: ");
-// int xb = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите знакчение координаты Y для точки B: ");
-// int yb = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите значение координаты Z для точки B: ");
-// int zb = int.Parse(Console.ReadLine()!);
-// double ab = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
-// Console.WriteLine($"Расстояние между точками AB = {ab:f2}");
+Console.WriteLine("---------------Задача №2---------------");
+//Расстояние между точками
+Console.WriteLine("Введите значение координаты X для точки А: ");
+int xa = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите знакчение координаты Y для точки А: ");
+int ya = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты Z для точки А: ");
+int za = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты X для точки B: ");
+int xb = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите знакчение координаты Y для точки B: ");
+int yb = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение координаты Z для точки B: ");
+int zb = int.Parse(Console.ReadLine()!);
+double ab = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
+Console.WriteLine($"Расстояние между точками AB = {ab:f2}");
 
 Console.WriteLine("---------------Задача №3---------------");
 //Таблица кубов
